9dfacbeaeea5094359e32614317533cedffdce0c
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-03-06 14:15:50 -0600

    Create prac4 ej2.c

---
+++ prac4 ej2.c
@@ -0,0 +1,20 @@
+//Salgado Perez Janis Lizeth
+// ejercicio 2
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+        int a=3;
+        int b=2;
+        
+        if (a>b)
+        {
+            printf ("a es mayor \n");
+        }
+        else
+        {
+            printf ("b es mayor o igual a \n");
+        }
+        return 0;
+} 
