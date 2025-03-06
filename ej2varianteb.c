0a6bb2537d06de2dba168b2b00f1341e6659add3
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-03-06 14:19:52 -0600

    Create ej2varianteb.c

---
+++ ej2varianteb.c
@@ -0,0 +1,22 @@
+//Salgado Perez Janis Lizeth
+// ejercicio 1 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+        int a=3;
+        int b=4;
+        
+        if (a>b)
+        {
+            printf ("a es mayor \n");
+        }
+        
+        else 
+        {
+            printf ("b es mayor \n");
+        }
+        return 0;
+    
+}
